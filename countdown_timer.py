a37ce2050ff7d077033608574bd00692608cac50
gwattanasuwan697 <45600371+gwattanasuwan697@users.noreply.github.com>
2018-12-05 10:53:56 -0600

    Update countdown_timer.py

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,4 +1,5 @@
 from sense_hat import SenseHat
+import time
 
 sense = SenseHat()
 
@@ -17,12 +18,14 @@ color = (0,0,255)
 
 pixels = []
 
-for y in range(1,9): #Set Pixels
-    for x in range(0,8):
-        pixel = numbers[((y-1)*8)+x]
-        sense.set_pixel(x, y-1, pixel*color[0], pixel*color[1], pixel*color[2])
+def show_pixel(numbers):
+  for y in range(1,9): #Set Pixels
+      for x in range(0,8):
+          pixel = numbers[((y-1)*8)+x]
+          sense.set_pixel(x, y-1, pixel*color[0], pixel*color[1], pixel*color[2])
 
-DIGIT = [0,0,1,0,0,1,0,0,1,0,0,1,0,0,1, #1
+DIGIT = [1,1,1,1,0,1,1,0,1,1,0,1,1,1,1, #0
+         0,0,1,0,0,1,0,0,1,0,0,1,0,0,1, #1
          1,1,1,0,0,1,1,1,1,1,0,0,1,1,1, #2
          1,1,1,0,0,1,1,1,1,0,0,1,1,1,1, #3
          1,0,1,1,0,1,1,1,1,0,0,1,0,0,1, #4
@@ -31,23 +34,14 @@ DIGIT = [0,0,1,0,0,1,0,0,1,0,0,1,0,0,1, #1
          1,1,1,0,0,1,0,0,1,0,0,1,0,0,1, #7
          1,1,1,1,0,1,1,1,1,1,0,1,1,1,1, #8
          1,1,1,1,0,1,1,1,1,0,0,1,1,1,1] #9
-
-'''def dotTimer(n): # Dot Timer List Creator
-  temp = []
-  for i in range(0,n):
-    temp.append(1)
-  n = abs(n-16)
-  for i in range(0,n):
-    temp.append(0)
-  return temp'''
          
 # MAIN         
          
 print("Timer (MAX. 960 secs)")
 time = int(input("Set timer (sec): "))
 
-while(true):
-    sec = 0
+sec = 0
+while(time > 0):
     
     if not time <= 60: # Time more than 60 / Dot Timer Required
         dot = time / 60
@@ -55,23 +49,26 @@ while(true):
     else:
         dotTimerPixel = [0]*16 # Time less than or equal to 60
     
-    time -= 60
-    sec += 60
+    if time % 60 == 0:
+      time -= 60
+      sec += 60
+    else:
+      sec = time % 60
     
     first_digit = int(sec / 10)
     second_digit = sec-(int(sec/10)*10)
     
-    
     numbers = []
-    for i in range(0,10):
+    for j in range(0,5):
         temp = []
-        for j in range(0,4):
-            temp += [0]
-            temp_digit = DIGIT[15*]
+        temp_digit_first = DIGIT[15*first_digit + (j*3) : 15*first_digit + (j*3) +3]
+        temp_digit_second = DIGIT[15*second_digit + (j*3) : 15*second_digit + (j*3) +3]
+        temp = temp + [0] + temp_digit_first + [0] + temp_digit_second
+        numbers += temp
     
+    sec -= 1
+    time -= 1
   
-pixels = dotTimerPixel + [0]*8 + numbers
-
-
-        
-
+    pixels = dotTimerPixel + [0]*8 + numbers
+    show_pixel(pixels)
+    time.sleep(1)

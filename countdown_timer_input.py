818be29cb8803ec46683d41da00be140e99018b9
gwattanasuwan697 <45600371+gwattanasuwan697@users.noreply.github.com>
2018-12-06 10:44:45 -0600

    Update countdown_timer_input.py

--- countdown_timer_input.py
+++ countdown_timer_input.py
@@ -49,15 +49,76 @@ def show_numbers(num):
     show_pixels(pixels)
     
 min_sel = 0
+show_numbers(min_sel)
 
-while(True):
+c = True
+while(c):
     for event in sense.stick.get_events():
         if event.action == "pressed":
-            if event.direction == "up": #Add overflow detection
-                min_sel += 1
+            if event.direction == "up":
+                if min_sel < 16:
+                    min_sel += 1
             elif event.direction == "down":
-                min_sel -= 1
+                if min_sel > 0:
+                    min_sel -= 1
             elif event.direction == "middle":
-                pass
+                c = False
+                break
         show_numbers(min_sel)
-            
+        
+sec_sel = 0
+show_numbers(sec_sel)
+
+c = True
+while(c):
+    for event in sense.stick.get_events():
+        if event.action == "pressed":
+            if event.direction == "up":
+                if sec_sel < 59:
+                    sec_sel += 1
+            elif event.direction == "down":
+                if sec_sel > 0:
+                    sec_sel -= 1
+            elif event.direction == "middle":
+                c = False
+                break
+        show_numbers(sec_sel)
+        
+time = (min_sel*60) + sec_sel
+
+sec = 0
+while(time > 0):
+    
+    if time % 60 == 0:
+      sec += 60
+    else:
+      sec = time % 60
+    
+    if not time <= 60: # Time more than 60 / Dot Timer Required
+        dot = int(time / 60)
+        dotTimerPixel = ([1]*dot) + ([0]*(16-dot))
+    else:
+        dotTimerPixel = [0]*16 # Time less than or equal to 60
+    
+    digit = get_digit(sec)
+    
+    numbers = []
+    for j in range(0,5):
+        temp = []
+        if digit[0] != 0:
+            temp_digit_first = DIGIT[15*digit[0] + (j*3) : 15*digit[0] + (j*3) +3]
+        else:
+            temp_digit_first = [0] * 3
+        temp_digit_second = DIGIT[15*digit[1] + (j*3) : 15*digit[1] + (j*3) +3]
+        temp = temp + [0] + temp_digit_first + [0] + temp_digit_second
+        numbers += temp
+    
+    sec -= 1
+    time -= 1
+  
+    pixels = dotTimerPixel + [0]*8 + numbers
+    show_pixels(pixels)
+    sleep(1)
+           
+smile = [0]*8 + ([0,1,1,0]*4) + [0]*8 + ([0,1,1,0]*2) + [0] + [1]*6 + [0]*3 + [1]*4 + [0]*10
+show_pixels(smile)
